86e7ed7d6b1c3df519e5c3541c32bf87981f8de8
ydfdas1f546g1df <78973743+ydfdas1f546g1df@users.noreply.github.com>
2025-08-19 20:02:17 +0200

    Innit Project

---
+++ Plugins/ImageOverlayPlugin/ImageOverlayPlugin.cs
@@ -0,0 +1,17 @@
+﻿using MissionPlanner.Plugin;
+
+namespace ImageOverlayPlugin
+{
+    internal class ImageOverlayPlugin : Plugin
+    {
+        public override string Name { get; } = "ImageOverlayPlugin";
+        public override string Version { get; } = "0.1";
+        public override string Author { get; } = "IDPA";
+        public override bool Init() { return true; }
+        public override bool Loaded()
+        {
+            return true;
+        }
+        public override bool Exit() { return true; }
+    }
+}
